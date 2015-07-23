121cd0cb1e3d6308847727b0853517f93775350c
Licht Takeuchi <licht-t@math.dis.titech.ac.jp>
2015-07-23 09:43:56 +0900

    Revert "Revert "Changed Settings""
    This reverts commit bf6c4ae79689a7cbebc0eeeec4700201c7ca66f6.

--- Display.cu
+++ Display.cu
@@ -6,172 +6,172 @@ void reshape(int w,int h);
 void idle();
 
 float getMaxPhero(){
-	float max = -1;
-	for (int i=0; i<MAX; i++){
-		for (int j=0; j<MAX; j++){
-			float p = cells[i][j].phero;
-			if (max<p){
-				max=p;
-			}
-		}
-	}
-	return max;
+    float max = -1;
+    for (int i=0; i<MAX; i++){
+        for (int j=0; j<MAX; j++){
+            float p = cells[i][j].phero;
+            if (max<p){
+                max=p;
+            }
+        }
+    }
+    return max;
 }
 
 
 void getHeatMapColor(float value, float cl[3]){
-	const int NUM_COLORS = 4;
-	static float color[NUM_COLORS][3] = { {0,0,1}, {0,1,0}, {1,1,0}, {1,0,0} };
-	// A static array of 4 colors:  (blue,   green,  yellow,  red) using {r,g,b} for each.
+    const int NUM_COLORS = 4;
+    static float color[NUM_COLORS][3] = { {0,0,1}, {0,1,0}, {1,1,0}, {1,0,0} };
+    // A static array of 4 colors:  (blue,   green,  yellow,  red) using {r,g,b} for each.
 
-	int idx1;        // |-- Our desired color will be between these two indexes in "color".
-	int idx2;        // |
-	float fractBetween = 0;  // Fraction between "idx1" and "idx2" where our value is.
+    int idx1;        // |-- Our desired color will be between these two indexes in "color".
+    int idx2;        // |
+    float fractBetween = 0;  // Fraction between "idx1" and "idx2" where our value is.
 
-	if(value <= 0){
-		idx1 = idx2 = 0;
-	}    // accounts for an input <=0
-	else if(value >= 1){
-		idx1 = idx2 = NUM_COLORS-1;
-	}    // accounts for an input >=0
-	else{
-		value = value * (NUM_COLORS-1);        // Will multiply value by 3.
-		idx1  = floor(value);                  // Our desired color will be after this index.
-		idx2  = idx1+1;                        // ... and before this index (inclusive).
-		fractBetween = value - float(idx1);    // Distance between the two indexes (0-1).
-	}
+    if(value <= 0){
+        idx1 = idx2 = 0;
+    }    // accounts for an input <=0
+    else if(value >= 1){
+        idx1 = idx2 = NUM_COLORS-1;
+    }    // accounts for an input >=0
+    else{
+        value = value * (NUM_COLORS-1);        // Will multiply value by 3.
+        idx1  = floor(value);                  // Our desired color will be after this index.
+        idx2  = idx1+1;                        // ... and before this index (inclusive).
+        fractBetween = value - float(idx1);    // Distance between the two indexes (0-1).
+    }
 
-	cl[0] = (color[idx2][0] - color[idx1][0])*fractBetween + color[idx1][0];
-	cl[1] = (color[idx2][1] - color[idx1][1])*fractBetween + color[idx1][1];
-	cl[2] = (color[idx2][2] - color[idx1][2])*fractBetween + color[idx1][2];
+    cl[0] = (color[idx2][0] - color[idx1][0])*fractBetween + color[idx1][0];
+    cl[1] = (color[idx2][1] - color[idx1][1])*fractBetween + color[idx1][1];
+    cl[2] = (color[idx2][2] - color[idx1][2])*fractBetween + color[idx1][2];
 }
 
 void display(int argc, char *argv[]){
-	glutInitWindowSize(600, 600);
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DOUBLE);
-	glutCreateWindow("Ants");
-	glutDisplayFunc(displayFunc);
-	glutIdleFunc(idle);
-	glutReshapeFunc(reshape);
-	init();
-	glutMainLoop();
+    glutInitWindowSize(600, 600);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DOUBLE);
+    glutCreateWindow("Ants");
+    glutDisplayFunc(displayFunc);
+    glutIdleFunc(idle);
+    glutReshapeFunc(reshape);
+    init();
+    glutMainLoop();
 }
 
 void idle(void)
 {
-	glutPostRedisplay();
+    glutPostRedisplay();
 }
 
 void glDrawHexLine(double x,double y, double r){
-	glBegin(GL_LINE_LOOP);
-	glColor3f(1.0, 1.0, 1.0);
-	for (int i=0; i<6; i++){
-		glVertex2f(x + r*cos(i*M_PI/3), y + r*sin(i*M_PI/3));
-	}
-	glEnd();
+    glBegin(GL_LINE_LOOP);
+    glColor3f(1.0, 1.0, 1.0);
+    for (int i=0; i<6; i++){
+        glVertex2f(x + r*cos(i*M_PI/3), y + r*sin(i*M_PI/3));
+    }
+    glEnd();
 }
 
 void glDrawHex(double x,double y, double r, float rgb[3]){
-	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(rgb[0], rgb[1], rgb[2]);
-	glVertex2f(x, y);
-	for (int i=0; i<6; i++){
-		glVertex2f(x + r*cos(i*M_PI/3), y + r*sin(i*M_PI/3));
-	}
-	glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(rgb[0], rgb[1], rgb[2]);
+    glVertex2f(x, y);
+    for (int i=0; i<6; i++){
+        glVertex2f(x + r*cos(i*M_PI/3), y + r*sin(i*M_PI/3));
+    }
+    glEnd();
 }
 
 void glDrawNest(double x,double y,double r){
-	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex2f(x, y);
-	for (int i=0; i<=6; i++){
-		glVertex2f(x + r*cos(i*M_PI/3), y + r*sin(i*M_PI/3));
-	}
-	glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex2f(x, y);
+    for (int i=0; i<=6; i++){
+        glVertex2f(x + r*cos(i*M_PI/3), y + r*sin(i*M_PI/3));
+    }
+    glEnd();
 }
 
 void glDrawFood(double x,double y,double r){
-	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex2f(x, y);
-	for (int i=0; i<=6; i++){
-		glVertex2f(x + r*cos(i*M_PI/3), y + r*sin(i*M_PI/3));
-	}
-	glEnd();
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex2f(x, y);
+    for (int i=0; i<=6; i++){
+        glVertex2f(x + r*cos(i*M_PI/3), y + r*sin(i*M_PI/3));
+    }
+    glEnd();
 }
 
 void drawCells(){
-	float maxPhero = getMaxPhero();
-	for(int i=1; i<MAX; i++){
-		for(int j=1; j<MAX; j++){
-			double x = cells[i][j].cart.x;
-			double y = cells[i][j].cart.y;
-			float p = cells[i][j].phero;
-			float rgb[3];
-			if ( (cells[i][j].status&NEAR_NEST) != NORMAL_CELL ){
-				glDrawNest(x,y,1/sqrt(3));
-			}
-			else if ( (cells[i][j].status&NEAR_FOOD) != NORMAL_CELL ){
-				glDrawFood(x,y,1/sqrt(3));
-			}
-			else{
-				getHeatMapColor(p/maxPhero, rgb);
-				glDrawHex(x,y,1/sqrt(3),rgb);
-			}
-		}
-	}
+    float maxPhero = getMaxPhero();
+    for(int i=1; i<MAX; i++){
+        for(int j=1; j<MAX; j++){
+            double x = cells[i][j].cart.x;
+            double y = cells[i][j].cart.y;
+            float p = cells[i][j].phero;
+            float rgb[3];
+            if ( (cells[i][j].status&NEAR_NEST) != NORMAL_CELL ){
+                glDrawNest(x,y,1/sqrt(3));
+            }
+            else if ( (cells[i][j].status&NEAR_FOOD) != NORMAL_CELL ){
+                glDrawFood(x,y,1/sqrt(3));
+            }
+            else{
+                getHeatMapColor(p/maxPhero, rgb);
+                glDrawHex(x,y,1/sqrt(3),rgb);
+            }
+        }
+    }
 }
 
 void drawAnts(){
-	for(int k=1; k<NMAX; k++){
-		int i,j;
-		enum AntStatus s = ants[k].status;
-		i = ants[k].i;
-		j = ants[k].j;
-		double x,y;
-		x = cells[i][j].cart.x;
-		y = cells[i][j].cart.y;
+    for(int k=1; k<NMAX; k++){
+        int i,j;
+        enum AntStatus s = ants[k].status;
+        i = ants[k].i;
+        j = ants[k].j;
+        double x,y;
+        x = cells[i][j].cart.x;
+        y = cells[i][j].cart.y;
 
-		glPointSize(5);
-		glBegin(GL_POINTS);
-		switch (s){
-			case FORAGE:
-				glColor3f(0.0, 1.0, 0.0);
-				break;
-			case GOHOME:
-				glColor3f(1.0, 0.0, 0.0);
-				break;
-			case EMERGENCY:
-				glColor3f(1.0, 1.0, 1.0);
-				break;
-			default:
-				exit(1);
-		}
-		glVertex2f(x , y);
-		glEnd();
-	}
+        glPointSize(5);
+        glBegin(GL_POINTS);
+        switch (s){
+            case FORAGE:
+                glColor3f(0.0, 1.0, 0.0);
+                break;
+            case GOHOME:
+                glColor3f(1.0, 0.0, 0.0);
+                break;
+            case EMERGENCY:
+                glColor3f(1.0, 1.0, 1.0);
+                break;
+            default:
+                exit(1);
+        }
+        glVertex2f(x , y);
+        glEnd();
+    }
 }
 
 void displayFunc(void){
-	glClear(GL_COLOR_BUFFER_BIT);
-	calculation();
-	cudaMemcpyFromSymbol(cells,cells_d,MAX*MAX*sizeof(Cell),0);
-	cudaMemcpyFromSymbol(ants,ants_d,NMAX*sizeof(Ant),0);
-	drawCells();
-	drawAnts();
+    glClear(GL_COLOR_BUFFER_BIT);
+    calculation();
+    cudaMemcpyFromSymbol(cells,cells_d,MAX*MAX*sizeof(Cell),0);
+    cudaMemcpyFromSymbol(ants,ants_d,NMAX*sizeof(Ant),0);
+    drawCells();
+    drawAnts();
 
-	glutSwapBuffers();
+    glutSwapBuffers();
 }
 
 void init(){
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
 }
 
 void reshape(int w, int h){
-	glMatrixMode(GL_PROJECTION);
-	glViewport(0, 0, w, h);
-	glLoadIdentity();
-	gluOrtho2D(-(CART_X_ZERO+1),(MAX-CART_X_ZERO)+1,-(CART_Y_ZERO+1),(MAX-CART_Y_ZERO)+1);
+    glMatrixMode(GL_PROJECTION);
+    glViewport(0, 0, w, h);
+    glLoadIdentity();
+    gluOrtho2D(-(CART_X_ZERO+1),(MAX-CART_X_ZERO)+1,-(CART_Y_ZERO+1),(MAX-CART_Y_ZERO)+1);
 }
--- main.cu
+++ main.cu
@@ -24,60 +24,60 @@
 
 int main(int argc, char *argv[]){
 
-	getHoming homeOp;
-	thrust::plus<int> binary_op;
+    getHoming homeOp;
+    thrust::plus<int> binary_op;
 
-	IOInit();
+    IOInit();
 
-	initialize();
+    initialize();
 
 
-	double normalSum = 0.0;
+    double normalSum = 0.0;
 
-	for(unsigned long long int dummy=1; dummy<=MAX_STEP; dummy++){
-		reset(1,0,dummy);
+    for(unsigned long long int dummy=1; dummy<=MAX_STEP; dummy++){
+        reset(1,0,dummy);
 
-		//cudaDeviceSynchronize();
-		//display(argc,argv);
+        //cudaDeviceSynchronize();
+        //display(argc,argv);
 
-		for(int t=0; t<MAX_TIME; t++){
-			calculation();
-			if(t%500==0){
-				IOEffPoll(0,500,dummy,t);
-			}
-		}
+        for(int t=0; t<MAX_TIME; t++){
+            calculation();
+            if(t%500==0){
+                IOEffPoll(0,500,dummy,t);
+            }
+        }
 
-		normalSum += thrust::transform_reduce(ants_d_ptr, ants_d_ptr+NMAX, homeOp, 0, binary_op);
-	}
+        normalSum += thrust::transform_reduce(ants_d_ptr, ants_d_ptr+NMAX, homeOp, 0, binary_op);
+    }
 
-	for (int n=0; n<=500; n+=50){
-		IOEffWrite(0,n,normalSum);
-	}
+    for (int n=0; n<=500; n+=50){
+        IOEffWrite(0,n,normalSum);
+    }
 
-	for (int pw=1; pw<=7; pw++){
-		for (int n=0; n<=450; n+=50){
-			double sensor = pow(10,-pw);
-			int naho = (NMAX - n);
+    for (int pw=1; pw<=7; pw++){
+        for (int n=0; n<=450; n+=50){
+            double sensor = pow(10,-pw);
+            int naho = (NMAX - n);
 
-			double sum = 0.0;
-			for(unsigned long long int dummy=1; dummy<=MAX_STEP; dummy++){
-				reset(sensor,naho,dummy);
+            double sum = 0.0;
+            for(unsigned long long int dummy=1; dummy<=MAX_STEP; dummy++){
+                reset(sensor,naho,dummy);
 
-				//display(argc,argv);
-				for(int t=1; t<=MAX_TIME; t++){
-					calculation();
-					if(t%500==0){
-						IOEffPoll(pw,n,dummy,t);
-					}
-				}
+                //display(argc,argv);
+                for(int t=1; t<=MAX_TIME; t++){
+                    calculation();
+                    if(t%500==0){
+                        IOEffPoll(pw,n,dummy,t);
+                    }
+                }
 
-				sum += thrust::transform_reduce(ants_d_ptr, ants_d_ptr+NMAX, homeOp, 0, binary_op);
-			}
-			IOCellWrite(pw,n);
-			IOEffWrite(pw,n,sum);
-		}
-		IOEffWrite(pw,500,normalSum);
-	}
+                sum += thrust::transform_reduce(ants_d_ptr, ants_d_ptr+NMAX, homeOp, 0, binary_op);
+            }
+            // IOCellWrite(pw,n);
+            IOEffWrite(pw,n,sum);
+        }
+        IOEffWrite(pw,500,normalSum);
+    }
 
 }
 
